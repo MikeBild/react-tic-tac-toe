f17d34ce6a518165a83eb0293cd0f59ae9ab9de6
Mike Bild <mike.bild@googlemail.com>
2017-06-07 08:30:26 +0200

    Cleanup and moved links

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 > an implementation of non-recursive [minimax alpha-beta pruning](https://en.wikipedia.org/wiki/Alpha%E2%80%93beta_pruning) inspirated by [Carsten Koenig](https://github.com/CarstenKoenig) awesome [Othello Game in Elm](https://github.com/CarstenKoenig/ElmOthello). Thank you Carsten!
 
-__[Live Demo](http://tic-tac-toe.services.dropstack.run)__
+__[Live Demo](http://tic-tac-toe.cloud.dropstack.run)__
 
 ## Setup
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const  webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
 
 module.exports = {
   entry: './src/app.js',
   output: {
-    publicPath: '/'
+    publicPath: '/',
   },
   module: {
     preLoaders: [{
       test: /\.js$/,
       loader: 'eslint',
-      exclude: /node_modules/
+      exclude: /node_modules/,
     }],
     loaders: [{
       test: /\.css/,
@@ -18,21 +18,21 @@ module.exports = {
     }, {
       test: /\.json$/,
       loader: 'json',
-      exclude: /node_modules/
+      exclude: /node_modules/,
     }, {
       test: /\.js$/,
       loader: 'babel',
-      exclude: /node_modules/
-    }]
+      exclude: /node_modules/,
+    }],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'src/index.html'
+      template: 'src/index.html',
     }),
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: JSON.stringify(process.env.NODE_ENV),
       },
     }),
-  ]
-};
+  ],
+}
